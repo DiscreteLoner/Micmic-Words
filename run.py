205453cdf32f8e65796a49577c956a9f06e7aef6
DiscreteLoner <45715773+DiscreteLoner@users.noreply.github.com>
2018-12-13 21:55:58 +0700

    Add files via upload
    Update!

--- run.py
+++ run.py
@@ -21,47 +21,45 @@ books_catalog = {
 "No Hero" : "https://www.gutenberg.org/files/11153/11153-h/11153-h.htm"
 }
 
-print ("Hello, welcome to E. W. Hornung quote generator.")
+print "Hello, welcome to E. W. Hornung quote generator."
 
 num_quote = ""
 book = ""
 
 while book != "A" and book != "B":
-  print ("")
-  print ("Which book do you want to micmic?")
-  print ("A = Dead Men Tell No Tale")
-  print ("B = No Hero")
+  print ""
+  print "Which book do you want to micmic?"
+  print "A = Dead Men Tell No Tale"
+  print "B = No Hero"
   book = raw_input("I choose: ")
 
-while num_quote != "1" and num_quote != "2" and num_quote != "3" and num_quote != "4" and num_quote != "5":
-  print ("")
-  print ("How many passages would you like to generate(1-5)")
-  num_quote = raw_input("?: ")
-
-num_quote = int(num_quote)
+while num_quote not in range (1, 6):
+    print ""
+    print "How many passages would you like to generate(1-5)"
+    num_quote = int(raw_input("?: "))
 
 length = 0
-while length < 3 or length > 100: 
+while length not in range(3, 101): 
   try:	
-    print ("")
-    print ("Words per passage (minimum of 3, maximum of 100):")
+    print ""
+    print "Words per passage (minimum of 3, maximum of 100):"
     length = raw_input("?: ")
     length = int(length)
   except ValueError:
-    print ("")
-    print ("Sorry, that's not a valid length.")
-    print ("")
-    print ("Please, try again!")
+    print ""
+    print "Sorry, that's not a valid length."
+    print ""
+    print "Please, try again!"
     length = 0
 
-print ("")
-print ("Accessing works... ")
+print ""
+print "Accessing works... "
 for link in books_catalog:
   if book == "A":
-    print ("Dead Men Tell No Tale")
+    print "Dead Men Tell No Tale"
     mc.add_string(fetch_data.fetch_web_data("https://www.gutenberg.org/files/1703/1703-h/1703-h.htm"))
   else:
-    print ("No Hero")
+    print "No Hero"
     mc.add_string(fetch_data.fetch_web_data("https://www.gutenberg.org/files/11153/11153-h/11153-h.htm"))
 
 """
@@ -73,7 +71,7 @@ for current in range(num_quote):
     try:
       temporary = mc.generate_text(length)
       formatted = " ".join(temporary)
-      print ("Passage #" + str(current+1) + ": " + formatted)
+      print "Passage #" + str(current+1) + ": " + formatted
       print
       break
     except UnicodeEncodeError:
